b14fdde6fc745578e7ede321034aa0a2f18db302
keepitrio <rio@knotch.com>
2021-12-06 23:03:46 -0500

    initial commit, project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TimeSlots from './components/TimeSlots';
+
+function App() {
+  return (
+    <div className="App">
+      <TimeSlots />
+    </div>
+  );
+}
+
+export default App;
